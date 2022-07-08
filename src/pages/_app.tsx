0172ae0b3028f116ef70984384207f24066f0875
Codiving <79081388+Codiving@users.noreply.github.com>
2022-07-08 20:35:11 +0900

    Add code to communicate with rn (#1)
    * Add code to communicate with rn
    
    * Move global.d.ts file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,49 @@
-import React, { useState } from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 
 import { GlobalStyle } from '@/theme';
+import { sendRNMessage } from '@/utils/rnMessage';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
+
+  const [name, setName] = useState<string>('김아무개');
+  const [type, setType] = useState<string>('type');
+
+  // React -> RN
+  useEffect(() => {
+    sendRNMessage({
+      type: 'NOTIFICATION',
+      data: {
+        name: '홍길동',
+      },
+    });
+  }, []);
+
+  // RN -> React
+  useEffect(() => {
+    if (!window.ReactNativeWebView) return;
+
+    const listener = (event: any) => {
+      const parsedData = JSON.parse(event.data);
+
+      const {
+        type,
+        data: { name },
+      } = parsedData;
+
+      setName(name);
+      setType(type);
+    };
+
+    window.addEventListener('message', listener);
+
+    return () => {
+      window.removeEventListener('message', listener);
+    };
+  }, []);
 
   return (
     <>
@@ -17,6 +54,8 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Hydrate state={pageProps.dehydratedState}>
           <Component {...pageProps} />
           <GlobalStyle />
+          <p style={{ margin: 100 }}> {type}</p>
+          <p style={{ margin: 100 }}> {name}</p>
         </Hydrate>
       </QueryClientProvider>
     </>
