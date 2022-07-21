387838019eb082509a6f8f9cde0d11fea588cb9b
hoo00nn <oh.jihoon@linecorp.com>
2022-07-22 00:57:21 +0900

    add example code for kakaoMapContainer usage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,52 +1,65 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 
 import { GlobalStyle } from '@/theme';
 import { sendRNMessage } from '@/utils/rnMessage';
 import { DatePicker } from '@/components';
+import KaKaoMapContainer from '@/components/commons/KaKaoMapContainer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
+  const kakaoMapRef = useRef<HTMLDivElement>(null);
 
-  const [name, setName] = useState<string>('김아무개');
-  const [type, setType] = useState<string>('type');
-
-  // React -> RN
+  // 사용 예시
   useEffect(() => {
-    sendRNMessage({
-      type: 'NOTIFICATION',
-      data: {
-        name: '홍길동',
-      },
-    });
+    if (window.kakao && kakaoMapRef.current) {
+      const options = {
+        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+      };
+      const map = new window.kakao.maps.Map(kakaoMapRef.current, options);
+    }
   }, []);
 
-  // RN -> React
-  useEffect(() => {
-    if (!window.ReactNativeWebView) return;
+  // const [name, setName] = useState<string>('김아무개');
+  // const [type, setType] = useState<string>('type');
 
-    const listener = (event: any) => {
-      const parsedData = JSON.parse(event.data);
+  // // React -> RN
+  // useEffect(() => {
+  //   sendRNMessage({
+  //     type: 'NOTIFICATION',
+  //     data: {
+  //       name: '홍길동',
+  //     },
+  //   });
+  // }, []);
 
-      const {
-        type,
-        data: { name },
-      } = parsedData;
+  // // RN -> React
+  // useEffect(() => {
+  //   if (!window.ReactNativeWebView) return;
 
-      setName(name);
-      setType(type);
-    };
+  //   const listener = (event: any) => {
+  //     const parsedData = JSON.parse(event.data);
 
-    window.addEventListener('message', listener);
+  //     const {
+  //       type,
+  //       data: { name },
+  //     } = parsedData;
 
-    return () => {
-      window.removeEventListener('message', listener);
-    };
-  }, []);
+  //     setName(name);
+  //     setType(type);
+  //   };
 
-  const [date, setDate] = useState(new Date());
+  //   window.addEventListener('message', listener);
+
+  //   return () => {
+  //     window.removeEventListener('message', listener);
+  //   };
+  // }, []);
+
+  // const [date, setDate] = useState(new Date());
 
   return (
     <>
@@ -57,9 +70,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Hydrate state={pageProps.dehydratedState}>
           <Component {...pageProps} />
           <GlobalStyle />
-          <p style={{ margin: 100 }}> {type}</p>
+          <KaKaoMapContainer ref={kakaoMapRef} width={500} height={500} />
+          {/* <p style={{ margin: 100 }}> {type}</p>
           <p style={{ margin: 100 }}> {name}</p>
-          <DatePicker date={date} onChange={(e) => setDate(e)} />
+          <DatePicker date={date} onChange={(e) => setDate(e)} /> */}
         </Hydrate>
       </QueryClientProvider>
     </>
