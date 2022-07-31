35031167f9ad93ffbc20c340bb1aa1a59e0764fb
Codiving <codivingmoon@gmail.com>
2022-07-31 12:32:17 +0900

    Add Typography ui

---
+++ src/ui/Typography/index.tsx
@@ -0,0 +1,27 @@
+import { shouldForwardProp } from '@/utils/emotion';
+import styled from '@emotion/styled';
+import { memo } from 'react';
+import { ContainerProps, TypographyProps } from './types';
+
+const Container = styled('span', {
+  shouldForwardProp: shouldForwardProp(['variant', 'weight']),
+})<ContainerProps>(({ theme, variant = 'body1', weight = 'regular' }) => {
+  const styles = theme.typography[variant];
+  const fontWeight = theme.typography.fontWeight[weight];
+  return {
+    ...styles,
+    fontWeight: fontWeight,
+    textShadow: variant === 'title1' && weight == 'bold' ? theme.typography.textShadow : undefined,
+  };
+});
+
+const Typography = (props: TypographyProps) => {
+  const { children, component, ...rest } = props;
+  return (
+    <Container as={component} {...rest}>
+      {children}
+    </Container>
+  );
+};
+
+export default memo(Typography);
