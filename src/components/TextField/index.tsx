ae56355e602e046e7c2af5af156c248266c0f7ad
Codiving <codivingmoon@gmail.com>
2022-07-31 20:21:16 +0900

    Add TextField component

---
+++ src/components/TextField/index.tsx
@@ -0,0 +1,69 @@
+import { shouldForwardProp } from '@/utils/emotion';
+import styled from '@emotion/styled';
+import { useState } from 'react';
+import { TextFieldProps } from './types';
+
+const Container = styled('div')<{ width?: React.CSSProperties['width'] }>(({ width }) => ({
+  position: 'relative',
+  display: 'inline-flex',
+  borderRadius: 10,
+  height: 60,
+  alignItems: 'center',
+  width,
+}));
+
+const Label = styled('label', { shouldForwardProp: shouldForwardProp(['focus']) })<{ focus: boolean }>(({ focus }) => ({
+  position: 'absolute',
+  left: 16,
+  fontSize: 12,
+  transition: '.2s',
+  pointerEvents: 'none',
+  top: focus ? 10 : (60 - 12) / 2,
+  color: '#79818F',
+}));
+
+const Input = styled('input', { shouldForwardProp: shouldForwardProp(['error', 'disabled']) })<{ error: boolean }>(
+  ({ theme, disabled, error }) => ({
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+    fontSize: 14,
+    padding: 16,
+    paddingTop: 24,
+    border: error ? `1px solid ${theme.color.textfield.error}` : `1px solid ${theme.color.textfield.normal}`,
+    '&:focus': {
+      border: disabled ? undefined : `1px solid ${theme.color.textfield.focus}`,
+    },
+    '&:active': {
+      border: disabled ? undefined : `1px solid ${theme.color.textfield.active}`,
+    },
+    '&:invalid': {
+      border: disabled ? undefined : `1px solid ${theme.color.textfield.error}`,
+    },
+  })
+);
+
+const TextField = (props: TextFieldProps) => {
+  const { label, value, onChange, error = false, disabled, width } = props;
+
+  const [focus, setFocus] = useState(value ? true : false);
+
+  return (
+    <Container
+      width={width}
+      onFocus={(e) => {
+        e.stopPropagation();
+        setFocus(true);
+      }}
+      onBlur={(e) => {
+        e.stopPropagation();
+        setFocus(value ? true : false);
+      }}
+    >
+      {label && <Label focus={focus}>{label}</Label>}
+      <Input value={value} onChange={onChange} disabled={disabled} error={error} />
+    </Container>
+  );
+};
+
+export default TextField;
---
+++ src/pages/textfield.tsx
@@ -0,0 +1,20 @@
+import TextField from '@/components/TextField';
+import { ChangeEvent, useState } from 'react';
+
+const TextFieldComponent = () => {
+  const [value, setValue] = useState('김유저');
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value);
+
+  return (
+    <>
+      <TextField width={220} value={value} onChange={onChange} label="Title" disabled />
+      <TextField value={value} onChange={onChange} label="Title" />
+      <TextField value={value} onChange={onChange} label="Title" />
+      <TextField value={value} onChange={onChange} label="Title" />
+      <TextField value={value} onChange={onChange} label="Title" error />
+    </>
+  );
+};
+
+export default TextFieldComponent;
