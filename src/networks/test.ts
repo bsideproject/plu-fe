d12b1795c43aa78c7caf2dab282dd9924d5f8b2c
hoo00nn <oh.jihoon@linecorp.com>
2022-07-31 13:01:28 +0900

    임시코드 수정

--- src/networks/test.ts
+++ src/networks/test.ts
@@ -1,8 +1,3 @@
-let count = 0;
-
 export const testRequest = () => {
-  if (typeof window === 'undefined') {
-    return 'hello, world s' + count++;
-  }
-  return 'hello, world c' + count++;
+  return 'hello, plu!';
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,13 @@ import { serviceQueryKey } from '@/commons/serviceQueryKey';
 import { testRequest } from '@/networks/test';
 import { useTestQuery } from '@/hooks/test';
 import { usePrefetchQuery } from '@/hooks/commons/prefetchQuery.hook';
-import { fetchAdminInfo } from '@/networks/admin';
 
 const MyStyleComponent = styled.div({ backgroundColor: 'red' });
 
 const Home: NextPage = () => {
   const { data } = useTestQuery();
 
-  return <MyStyleComponent>hello, bside {data}</MyStyleComponent>;
+  return <MyStyleComponent>{data}</MyStyleComponent>;
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
