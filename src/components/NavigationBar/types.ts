a2dcff002d2d6bb1bef2c7ef69d0e9f9dfae6c42
Codiving <codivingmoon@gmail.com>
2022-07-31 17:17:03 +0900

    Add NavigationBar component

---
+++ src/components/NavigationBar/types.ts
@@ -0,0 +1,9 @@
+interface NavigationBarProps {
+  startIcon?: React.ReactNode;
+  endIcon?: React.ReactNode;
+  onClickStartIcon?: () => void;
+  onClickEndIcon?: () => void;
+  title?: string;
+}
+
+export type { NavigationBarProps };
