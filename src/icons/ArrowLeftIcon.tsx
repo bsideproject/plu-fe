04955227131593de12e60a01851ea2c3bc1f9eea
Codiving <codivingmoon@gmail.com>
2022-07-31 17:16:27 +0900

    Add Icons

---
+++ src/icons/ArrowLeftIcon.tsx
@@ -0,0 +1,9 @@
+const ArrowLeftIcon = () => {
+  return (
+    <svg width="55" height="56" viewBox="0 0 55 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M31.6125 38L21.6125 28L31.6125 18L33.3875 19.775L25.1625 28L33.3875 36.225L31.6125 38Z" fill="black" />
+    </svg>
+  );
+};
+
+export default ArrowLeftIcon;
