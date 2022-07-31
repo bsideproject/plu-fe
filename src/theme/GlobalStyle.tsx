3c1f6407cbf94e11fa86fcb27ce7cffe8f735cf3
Codiving <codivingmoon@gmail.com>
2022-07-31 12:16:10 +0900

    Add Pretendard font

--- src/theme/GlobalStyle.tsx
+++ src/theme/GlobalStyle.tsx
@@ -7,6 +7,14 @@ const customReset = css`
   *::after {
     box-sizing: border-box;
   }
+
+  @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css');
+
+  body * {
+    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI',
+      'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif;
+    padding: 0 20px;
+  }
 `;
 
 const GlobalStyle = () => (
