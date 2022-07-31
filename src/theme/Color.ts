4ec50765e3cc1a1beaddf892d64bc03e5d67be59
Codiving <codivingmoon@gmail.com>
2022-07-31 20:21:10 +0900

    Add Color theme

--- src/theme/Color.ts
+++ src/theme/Color.ts
@@ -4,18 +4,35 @@ interface ButtonColor {
   disabled: string;
 }
 
-const BUTTON_COLOR = {
+const BUTTON_COLOR: ButtonColor = {
   active: '#19C885',
   disabled: '#E6E6E6',
+};
+
+// -------------------------------- TextFieldColor
+interface TextFieldColor {
+  focus: string;
+  active: string;
+  normal: string;
+  error: string;
+}
+
+const TEXTFIELD_COLOR: TextFieldColor = {
+  focus: '#19C885',
+  active: '#19C885',
+  normal: '#CCCCCC',
+  error: '#F1604C',
 };
 
 // -------------------------------- All Color
 interface Color {
   button: ButtonColor;
+  textfield: TextFieldColor;
 }
 
 const COLOR = {
   button: BUTTON_COLOR,
+  textfield: TEXTFIELD_COLOR,
 };
 
 export { COLOR };
