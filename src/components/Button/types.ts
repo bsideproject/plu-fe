4c42857da629073112168b8bd4816d20bcd499be
Codiving <codivingmoon@gmail.com>
2022-08-01 17:27:23 +0900

    Update Button component
    Add startIcon, endIcon

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -2,8 +2,14 @@ import React from 'react';
 
 interface ButtonProps extends React.ComponentProps<'button'> {
   width?: React.CSSProperties['width'];
+  children?: string;
   fullWidth?: boolean;
   variant?: 'active' | 'disabled';
+  startIcon?: React.ReactNode;
+  onClickStartIcon?: () => void;
+  endIcon?: React.ReactNode;
+  onClickEndIcon?: () => void;
+  bgColor?: React.CSSProperties['backgroundColor'];
 }
 
 export type { ButtonProps };
