322d5cc966b79e11c3704cdef82790288e660b37
Codiving <codivingmoon@gmail.com>
2022-08-01 17:26:51 +0900

    Add LoginIcon components

---
+++ src/icons/index.ts
@@ -0,0 +1,7 @@
+import AppleIcon from './AppleIcon';
+import ArrowLeftIcon from './ArrowLeftIcon';
+import KakaoIcon from './KakaoIcon';
+import NaverIcon from './NaverIcon';
+import SettingIcon from './SettingIcon';
+
+export { AppleIcon, ArrowLeftIcon, KakaoIcon, NaverIcon, SettingIcon };
