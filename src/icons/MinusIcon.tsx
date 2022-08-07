5c2f9f69f5e8d6df7318ccd4d6420b4570a1343b
Codiving <codivingmoon@gmail.com>
2022-08-07 11:45:16 +0900

    Add Icons

---
+++ src/icons/MinusIcon.tsx
@@ -0,0 +1,10 @@
+const MinusIcon = () => {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="12" cy="12" r="11.5" fill="white" stroke="#333333" />
+      <rect x="7" y="11.5" width="10" height="1" fill="#333333" />
+    </svg>
+  );
+};
+
+export default MinusIcon;
--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,7 +1,9 @@
 import AppleIcon from './AppleIcon';
 import ArrowLeftIcon from './ArrowLeftIcon';
 import KakaoIcon from './KakaoIcon';
+import MinusIcon from './MinusIcon';
 import NaverIcon from './NaverIcon';
+import PlusIcon from './PlusIcon';
 import SettingIcon from './SettingIcon';
 
-export { AppleIcon, ArrowLeftIcon, KakaoIcon, NaverIcon, SettingIcon };
+export { AppleIcon, ArrowLeftIcon, KakaoIcon, NaverIcon, SettingIcon, PlusIcon, MinusIcon };
