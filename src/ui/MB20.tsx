7aa5a74f57cdfe70dd727930e3b7365b0398841d
Codiving <codivingmoon@gmail.com>
2022-08-07 11:30:31 +0900

    Add MB20

---
+++ src/ui/MB20.tsx
@@ -0,0 +1,5 @@
+import styled from '@emotion/styled';
+
+const MB20 = styled('div')(() => ({ marginBottom: 20 }));
+
+export default MB20;
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,4 +1,5 @@
 import Flex from './Flex';
 import FlexItem from './FlexItem';
+import MB20 from './MB20';
 
-export { Flex, FlexItem };
+export { Flex, FlexItem, MB20 };
