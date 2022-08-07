03afdc226dddb8acc240f7863fc55cf8a0793721
Codiving <codivingmoon@gmail.com>
2022-08-07 12:19:48 +0900

    Add ArowRightIcon

---
+++ src/icons/ArrowRightIcon.tsx
@@ -0,0 +1,9 @@
+const ArrowRightIcon = () => {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M8.5 5.5L15.5 12L8.5 18.5" stroke="#333333" />
+    </svg>
+  );
+};
+
+export default ArrowRightIcon;
--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,9 +1,10 @@
 import AppleIcon from './AppleIcon';
 import ArrowLeftIcon from './ArrowLeftIcon';
+import ArrowRightIcon from './ArrowRightIcon';
 import KakaoIcon from './KakaoIcon';
 import MinusIcon from './MinusIcon';
 import NaverIcon from './NaverIcon';
 import PlusIcon from './PlusIcon';
 import SettingIcon from './SettingIcon';
 
-export { AppleIcon, ArrowLeftIcon, KakaoIcon, NaverIcon, SettingIcon, PlusIcon, MinusIcon };
+export { AppleIcon, ArrowLeftIcon, KakaoIcon, NaverIcon, SettingIcon, PlusIcon, MinusIcon, ArrowRightIcon };
