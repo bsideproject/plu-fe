1b06d47cf23ed10192e4b88b7999d87607722c28
Codiving <codivingmoon@gmail.com>
2022-08-09 22:49:02 +0900

    Bugfix (crash build) and Add date text

--- src/pages/crew/create_gathering.tsx
+++ src/pages/crew/create_gathering.tsx
@@ -2,9 +2,10 @@ import { Button, Gathering, NavigationBar, Typography } from '@/components';
 import TextField from '@/components/TextField';
 import { ArrowLeftIcon, MinusIcon, PlusIcon } from '@/icons';
 import { Flex, MB20, Padding } from '@/ui';
+import CustomDate from '@/utils/CustomDate/CustomDate';
 import { shouldForwardProp } from '@/utils/emotion';
 import styled from '@emotion/styled';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 
 const label = 'CrewCreateGathering';
 
@@ -23,6 +24,9 @@ const IconButton = styled('div', {
 const CrewCreateGathering = () => {
   const [name, setName] = useState('');
   const [count, setCount] = useState(0);
+  const [date, setDate] = useState(new Date());
+
+  const dObject = new CustomDate(date);
 
   const onChangeName = (e: ChangeEvent<HTMLTextAreaElement>) => setName(e.currentTarget.value);
 
@@ -37,6 +41,19 @@ const CrewCreateGathering = () => {
   const onCountDown = () => setCount((prev) => (prev <= 1 ? prev : prev - 1));
 
   const onCountUp = () => setCount((prev) => (prev >= MAX_COUNT ? prev : prev + 1));
+
+  const onChangeDate = (date: Date) => setDate(date);
+
+  useEffect(() => {
+    const dObject = new CustomDate();
+    const minute = dObject.minute;
+
+    if (minute % 10) {
+      const newMinute = (parseInt(String(minute / 10)) + 1) * 10;
+      const needAddMinute = newMinute - minute;
+      setDate(dObject.add(needAddMinute, 'minute').getDateObject);
+    }
+  }, []);
 
   return (
     <Flex label={label} direction="column" justifyContent="space-between" fullHeight>
@@ -73,9 +90,9 @@ const CrewCreateGathering = () => {
           </Typography>
         </Padding>
         <Padding>
-          <Gathering label1="시간" />
-          <Gathering label1="날짜" />
-          <Gathering label1="장소" bb={false} />
+          <Gathering label="시간" text={dObject.format('A HH:mm')} />
+          <Gathering label="날짜" text={dObject.format('YYYY년 MM월 DD일')} />
+          <Gathering label="장소" bb={false} text="???" />
         </Padding>
       </div>
 
