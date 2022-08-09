42ddcaff714984a39d043379e54757f07aa50f22
Codiving <codivingmoon@gmail.com>
2022-08-09 22:39:03 +0900

    Update import fonts

--- src/theme/GlobalStyle.tsx
+++ src/theme/GlobalStyle.tsx
@@ -12,8 +12,6 @@ const customReset = css`
     outline: 0;
   }
 
-  @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css');
-
   body * {
     font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI',
       'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif;
