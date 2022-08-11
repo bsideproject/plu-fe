42346b0d9092a2e918b6b0a47ab12c323bbb1f97
Codiving <codivingmoon@gmail.com>
2022-08-11 23:24:53 +0900

    Add RemoveIcon

---
+++ src/icons/RemoveIcon.tsx
@@ -0,0 +1,14 @@
+const RemoveIcon = () => {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M11.2929 12L6.64648 16.6465L7.35359 17.3536L12 12.7071L16.6465 17.3536L17.3536 16.6465L12.7071 12L17.3536 7.35359L16.6465 6.64648L12 11.2929L7.35359 6.64648L6.64648 7.35359L11.2929 12Z"
+        fill="#19C885"
+      />
+    </svg>
+  );
+};
+
+export default RemoveIcon;
