fcd40189f6738b8a0d54bd6b83538507a8b4dc62
Codiving <codivingmoon@gmail.com>
2022-08-13 15:05:28 +0900

    Update NavigationBottom component

--- src/components/NavigationBottom/index.tsx
+++ src/components/NavigationBottom/index.tsx
@@ -1,26 +1,45 @@
+import { NavigationIcon } from '@/icons';
+import { Flex } from '@/ui';
 import { shouldForwardProp } from '@/utils/emotion';
 import styled from '@emotion/styled';
 import { useMemo } from 'react';
 import Typography from '../Typography';
-import { NavigationBottomProps } from './types';
+import { NavigationBottomData, NavigationBottomProps } from './types';
 
 const Container = styled('div', {
   shouldForwardProp: shouldForwardProp(['size']),
 })<{ size: number }>(({ size }) => ({
   display: 'grid',
   gridTemplateColumns: `repeat(${size}, 1fr)`,
+  borderTop: '1px solid #e6e6e6',
+  paddingBottom: 16,
 }));
 
-const Stack = styled('div')(() => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  height: 60,
-  cursor: 'pointer',
-}));
+const DATA: NavigationBottomData[] = [
+  {
+    icon: <NavigationIcon />,
+    text: '홈',
+  },
+  {
+    icon: <NavigationIcon />,
+    text: '랭킹',
+  },
+  {
+    icon: <NavigationIcon />,
+    text: '아티클',
+  },
+  {
+    icon: <NavigationIcon />,
+    text: '기록',
+  },
+  {
+    icon: <NavigationIcon />,
+    text: '마이',
+  },
+];
 
 const NavigationBottom = (props: NavigationBottomProps) => {
-  const { data = [] } = props;
+  const { data = DATA } = props;
 
   const size = useMemo(() => data.length, [data.length]);
 
@@ -29,10 +48,10 @@ const NavigationBottom = (props: NavigationBottomProps) => {
       {data.map((item, index) => {
         const { icon, text } = item;
         return (
-          <Stack key={index}>
+          <Flex key={index} direction="column" alignItems={'center'} justifyContent="space-around" height={60}>
             {icon}
-            <Typography>{text}</Typography>
-          </Stack>
+            <Typography color="#007AFF">{text}</Typography>
+          </Flex>
         );
       })}
     </Container>
