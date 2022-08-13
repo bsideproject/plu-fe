9391c21a4d9820b3cbb1c1b3ba666367c0dfb556
Codiving <codivingmoon@gmail.com>
2022-08-13 14:26:28 +0900

    Add MainLayout (padding)

---
+++ src/ui/MainLayout.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+const MainLayout = styled('div')(() => ({
+  padding: '0 20px 16px',
+  height: '100%',
+}));
+
+export default MainLayout;
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,6 +1,7 @@
 import Flex from './Flex';
 import FlexItem from './FlexItem';
+import MainLayout from './MainLayout';
 import MB20 from './MB20';
 import Padding from './Padding';
 
-export { Flex, FlexItem, MB20, Padding };
+export { Flex, FlexItem, MB20, Padding, MainLayout };
