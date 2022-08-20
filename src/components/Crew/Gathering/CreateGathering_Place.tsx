f194f1ae3c9b300d3bf911ed600be4d05c726ef9
hoo00nn <oh.jihoon@linecorp.com>
2022-08-20 21:47:52 +0900

    add search location

--- src/components/Crew/Gathering/CreateGathering_Place.tsx
+++ src/components/Crew/Gathering/CreateGathering_Place.tsx
@@ -3,8 +3,8 @@ import TextField from '@/components/TextField';
 import Typography from '@/components/Typography';
 import { ArrowLeftIcon, GpsIcon } from '@/icons';
 import { Place } from '@/pages/crew/create_gathering';
-import { Flex, FlexItem, MainLayout, MB20, Padding } from '@/ui';
-import { ChangeEvent } from 'react';
+import { Flex, FlexItem, MB20, Padding } from '@/ui';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 
 interface Props {
   place: Place;
@@ -12,57 +12,90 @@ interface Props {
   onPrev: () => void;
 }
 
-const DUMMY = [
-  { name: '트래비커피111', address: '서울 11', lon: 123123, lat: 123123 },
-  { name: '트래비커피222', address: '서울 22', lon: 123123, lat: 123123 },
-  { name: '트래비커피333', address: '서울 33', lon: 123123, lat: 123123 },
-];
-
 const CreateGathering_Place = (props: Props) => {
   const { onPrev, place, onChangePlace } = props;
+  const kakaoMapRef = useRef<HTMLDivElement>(null);
+  const kakakoPlaceRef = useRef<any>(null);
+  const [placeList, setPlaceList] = useState<Place[]>([]);
 
-  const onChangeName = (e: ChangeEvent<HTMLTextAreaElement>) => onChangePlace({ ...place, name: e.target.value });
+  const onChangeName = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    onChangePlace({ ...place, name: e.target.value });
+    searchKeyword(e.target.value);
+  };
+
+  useEffect(() => {
+    if (window.kakao && kakaoMapRef.current) {
+      const options = {
+        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+      };
+      const map = new window.kakao.maps.Map(kakaoMapRef.current, options);
+      const ps = new window.kakao.maps.services.Places();
+
+      kakakoPlaceRef.current = ps;
+
+      searchKeyword(place.name);
+    }
+  }, []);
+
+  const searchKeyword = (keyword: string) => {
+    if (kakakoPlaceRef.current) {
+      kakakoPlaceRef.current.keywordSearch(keyword, placesSearchCB);
+    }
+  };
+
+  function placesSearchCB(data, status, pagination) {
+    if (status === window.kakao.maps.services.Status.OK) {
+      setPlaceList([
+        ...data.map((v) => ({
+          lat: v.x,
+          lon: v.y,
+          name: v.place_name,
+          address: v.road_address_name,
+        })),
+      ]);
+    }
+  }
 
   return (
-    <MainLayout inner>
-      <Flex label={'label'} direction="column" justifyContent="space-between" fullHeight>
-        <div>
-          <MB20>
-            <NavigationBar onClickStartIcon={onPrev} startIcon={<ArrowLeftIcon />} title="장소" />
-          </MB20>
-          <MB20>
-            <TextField value={place.name} onChange={onChangeName} fullWidth label="장소" />
-          </MB20>
-          <MB20>
-            <Flex direction="column">
-              {DUMMY.map((item) => {
-                return (
-                  <FlexItem
-                    key={item.address}
-                    onClick={() => {
-                      onChangePlace(item);
-                      onPrev();
-                    }}
-                  >
-                    <Padding>
-                      <Flex gap={30} alignItems="center">
-                        <GpsIcon color="#CCCCCC" />
-                        <FlexItem>
-                          <Flex gap={10} direction="column">
-                            <Typography component="p">{item.name}</Typography>
-                            <Typography component="p">{item.address}</Typography>
-                          </Flex>
-                        </FlexItem>
-                      </Flex>
-                    </Padding>
-                  </FlexItem>
-                );
-              })}
-            </Flex>
-          </MB20>
-        </div>
-      </Flex>
-    </MainLayout>
+    <Flex label={'label'} direction="column" justifyContent="space-between" fullHeight>
+      <div ref={kakaoMapRef} style={{ display: 'none' }} />
+      <div>
+        <MB20>
+          <NavigationBar onClickStartIcon={onPrev} startIcon={<ArrowLeftIcon />} title="장소" />
+        </MB20>
+        <MB20>
+          <TextField value={place.name} onChange={onChangeName} fullWidth label="장소" />
+        </MB20>
+        <MB20>
+          <Flex direction="column">
+            {placeList.map((place) => {
+              return (
+                <FlexItem
+                  key={`${place.name}${place.address}`}
+                  onClick={() => {
+                    onChangePlace(place);
+                    onPrev();
+                  }}
+                >
+                  <Padding>
+                    <Flex gap={30} alignItems="center">
+                      <GpsIcon color="#CCCCCC" />
+                      <FlexItem>
+                        <Flex gap={10} direction="column">
+                          <Typography component="p">{place.name}</Typography>
+                          <Typography component="p">{place.address}</Typography>
+                        </Flex>
+                      </FlexItem>
+                    </Flex>
+                  </Padding>
+                </FlexItem>
+              );
+            })}
+          </Flex>
+        </MB20>
+      </div>
+    </Flex>
   );
 };
 
