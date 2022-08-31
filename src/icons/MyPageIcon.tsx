b4898f6eca23a331c7d9dee3f3abea693c04878b
Codiving <codivingmoon@gmail.com>
2022-08-31 23:22:27 +0900

    Add NavigationBottom icons

---
+++ src/icons/MyPageIcon.tsx
@@ -0,0 +1,18 @@
+const MyPageIcon = () => {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M12 12C15.3137 12 18 9.31371 18 6C18 2.68629 15.3137 0 12 0C8.68629 0 6 2.68629 6 6C6 9.31371 8.68629 12 12 12Z"
+        fill="#666666"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M12 14C18.0751 14 23 16.9249 23 23C23 23.3539 22.9253 23.6872 22.7847 23.9998H1.21527C1.07466 23.6872 1 23.3539 1 23C1 16.9249 5.92487 14 12 14Z"
+        fill="#666666"
+      />
+    </svg>
+  );
+};
+
+export default MyPageIcon;
--- src/icons/index.ts
+++ src/icons/index.ts
@@ -3,20 +3,30 @@ import ArrowDownIcon from './ArrowDownIcon';
 import ArrowLeftIcon from './ArrowLeftIcon';
 import ArrowRightIcon from './ArrowRightIcon';
 import ArrowUpIcon from './ArrowUpIcon';
+import ArticleIcon from './ArticleIcon';
 import GpsIcon from './GpsIcon';
+import HomeIcon from './HomeIcon';
 import KakaoIcon from './KakaoIcon';
 import LogoIcon from './LogoIcon';
 import MinusIcon from './MinusIcon';
+import MyPageIcon from './MyPageIcon';
 import NaverIcon from './NaverIcon';
 import NavigationIcon from './NavigationIcon';
 import PersonIcon from './PersonIcon';
 import PlusIcon from './PlusIcon';
 import ProfileIcon from './ProfileIcon';
+import RankIcon from './RankIcon';
+import RecordIcon from './RecordIcon';
 import SettingIcon from './SettingIcon';
 import ToastConfirmIcon from './ToastConfirmIcon';
 import ToastErrorIcon from './ToastErrorIcon';
 
 export {
+  ArticleIcon,
+  HomeIcon,
+  MyPageIcon,
+  RankIcon,
+  RecordIcon,
   AppleIcon,
   ArrowDownIcon,
   ArrowLeftIcon,
