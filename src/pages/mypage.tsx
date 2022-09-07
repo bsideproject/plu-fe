9619caf9f35e681ae56852f2cd4fb49bb02e42c8
Codiving <codivingmoon@gmail.com>
2022-09-07 22:22:24 +0900

    Revert "Add dummy data"
    This reverts commit 74773e26f62972895271ffc4b693f0e89c08cab8.

--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -25,34 +25,7 @@ const MyPage = () => {
         <Container>
           <MyFeedCommon profileImage="나중에이미지넣어야함" name="김유저" />
           <Tab tabPadding={8} tabNames={TAB_NAME} tabIndex={tabIndex} onChangeTabIndex={onChangeTabIndex} />
-          {tabIndex === 0 && (
-            <MyFeed
-              images={[
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-                'https://images.unsplash.com/photo-1656595640267-3891165159b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=900&q=60',
-              ]}
-            />
-          )}
+          {tabIndex === 0 && <MyFeed images={[]} />}
           {tabIndex === 1 && (
             <>
               {/* TODO: 추후 실 데이터 넣어야 함 */}
